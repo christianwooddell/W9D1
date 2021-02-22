4995bf89daaa0a0df181a29be92df12d516a9279
yohan oh <yohan.oh332@gmail.com>
2021-02-22 16:40:18 -0500

    worked on function prototype.curry

--- argumentsExercise.js
+++ argumentsExercise.js
@@ -34,29 +34,88 @@ class Dog {
         this.name = name;
     }
 }
-Function.prototype.myBind = function (ctx) {
-    const that = this;
-    const bindTimeArgs = Array.from(arguments).slice(1)
-    console.log(bindTimeArgs)
-    return function(){
-        const callTimeArgs = Array.from(arguments)
-        console.log(callTimeArgs)
-        const allArgs = bindTimeArgs.concat(callTimeArgs)
-        console.log(allArgs)
-        that.apply(ctx, allArgs)
-    }
-}
 
 const spots = new Cat("Spots");
 const fido = new Dog("Fido");
 
-// spots.says("meow", "Rich");
-let notSpotSays = spots.says.myBind(fido, "meow");
-notSpotSays("Rich");
-notSpotSays("Yohan");
+// Function.prototype.myBind = function (ctx, ...args) {
+//     const that = this;
+   
+//     return function(){
+   
+//         that.apply(ctx, args)
+//     }
+// }
+// spots.says.myBind(fido, "meow", "Christian")();
 
 
-function Surrogate() {};
-Surrogate.prototype = Dog.prototype;
-Cat.prototype = new Surrogate();
-Cat.prototype.constructor = Cat;
+// Function.prototype.myBind = function (ctx) {
+//     const that = this;
+   
+//     return function(){
+//         const callTimeArgs = Array.from(arguments)
+//         that.apply(ctx, callTimeArgs)
+//     }
+// }
+// spots.says.myBind(fido)("meow","Christian")
+
+
+// Function.prototype.myBind = function (ctx) {
+//     const that = this;
+//     const bindTimeArgs = Array.from(arguments).slice(1)
+//     return function(){
+//         const callTimeArgs = Array.from(arguments)
+//         const allTimeArgs = bindTimeArgs.concat(callTimeArgs)
+//         that.apply(ctx, allTimeArgs)
+//     }
+// }
+
+// spots.says.myBind(fido, "meow")("Yohan");
+
+// let notSpotSays = spots.says.myBind(fido, "meow");
+// notSpotSays("Rich");
+// notSpotSays("Dane");
+
+function curriedSum(numArgs) {
+    const numbers = [];
+   
+    function _curriedSum(num) {
+        numbers.push(num);
+        let total = 0;
+        if (numbers.length === numArgs) {
+            numbers.forEach((num) => { total += num });
+            return total;
+        } else {
+            return _curriedSum
+        }
+    };
+    return _curriedSum; 
+}
+
+console.log(curriedSum(3)(1)(2)(5));
+// curry definition =  
+// (logFunc).curry
+Function.prototype.curry = function(numArgs) {
+    const args = [];
+    const that = this; 
+
+    function _curry(ele) {
+        args.push(ele);
+
+        if (args.length === numArgs) {
+            that.apply(that, args)
+//          (logFunc).apply(that,args)<-- all the passed in arguments gets invoked once args reachs numArgs
+        } else {
+            return _curry
+        }
+    }
+    return _curry
+    
+}
+
+let logFunc = function(...args) {
+    for (let i = 0; i < args.length; i++) {
+      console.log(args[i]);
+    }
+};
+  
